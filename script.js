a7c2e048067987d58255f5b7f349ae5a59fe0f7a
mullerow <maximilian.kohler@posteo.de>
2024-03-04 10:30:12 +0100

    feat: all checkboxes can't be longer checked at the same time

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function passwordFunction() {
 
 button.addEventListener("click", passwordFunction);
 */
+
+////// fast, cheap and good??? //////////////
+
+const main = document.querySelector("main");
+const fast = main.querySelector("#fast");
+const cheap = main.querySelector("#cheap");
+const good = main.querySelector("#good");
+let penultimateCheck;
+let lastCheck;
+let currentCheck;
+
+function checkboxCheck(event) {
+  currentCheck = event.target;
+  penultimateCheck = lastCheck;
+  lastCheck = currentCheck;
+
+  console.log(" lastCheck", lastCheck);
+  console.warn("  penultimateCheck", penultimateCheck);
+
+  if (fast.checked && cheap.checked && good.checked) {
+    penultimateCheck.checked = false;
+  }
+}
+
+fast.addEventListener("click", checkboxCheck);
+
+cheap.addEventListener("click", checkboxCheck);
+
+good.addEventListener("click", checkboxCheck);
