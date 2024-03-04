cea37a3e13554b4135cb16d1ef069690f979e069
mullerow <maximilian.kohler@posteo.de>
2024-03-04 09:47:14 +0100

    add: the form is now styled

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+const main = document.querySelector("main");
+const input = main.querySelector(".input-class");
+const button = main.querySelector("#show-button");
+let password = "";
+function passwordFunction() {
+  password = input.value;
+  console.log("password", password);
+}
+
+button.addEventListener("click", passwordFunction);
+*/
